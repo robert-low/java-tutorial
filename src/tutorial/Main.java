1d6d9f3d48dc4fc5add0f8e1590a5363ee26c61b
Robert Low <robert.low91@gmail.com>
2023-04-12 19:27:06 +0100

    hello world

--- src/tutorial/Main.java
+++ src/tutorial/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//Print to terminal
+		System.out.println("Hello World!");
+		System.out.println("Hello World 2");
 	}
 
 }
